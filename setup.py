f851b58258f3ec26f7b6b2c9f2349b90f1974a3b
Joseph Greene <jgreenemi@gmail.com>
2017-12-22 21:10:11 -0800

    Create proper test check process.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ def lambda_packer(config={}):
         # for the Lambda to function, but in case you have custom needs you can add the necessary directories under this
         # dict.
         directories_to_include = ['config', 'src']
+        destination_filepath = 'target/lambdapack-{}.zip'.format(config['training-job-name'])
 
         # Create the target directory if it doesn't exist.
         if not os.path.exists('target'):
@@ -33,7 +34,7 @@ def lambda_packer(config={}):
             except:
                 raise Exception('Tried to create target/ dir but failed.')
 
-        with zipfile.ZipFile('target/lambdapack-{}.zip'.format(config['training-job-name']), mode='w') as lambdapack:
+        with zipfile.ZipFile(destination_filepath, mode='w') as lambdapack:
             # Crawl directories, and write their files into the ZIP with the structure of foldername/filename.
             # This should maintain subdirectory heirarchies.
             for dirname in directories_to_include:
@@ -43,12 +44,13 @@ def lambda_packer(config={}):
                         logging.warning('Adding: {}'.format(composite_filename))
                         lambdapack.write(filename=composite_filename, arcname=composite_filename)
 
-        logging.info('Packed lambdapack to target/lambdapack-{}.zip'.format(config['training-job-name']))
-        return True
+        logging.info('Packed lambdapack to {}'.format(destination_filepath))
+        return True, destination_filepath
 
     except Exception as e:
-        logging.error('lambda_packer failure: {}'.format(e))
-        return False
+        msg = 'lambda_packer failure: {}'.format(e)
+        logging.error(msg)
+        return False, msg
 
 
 def lambda_creation(config={}):
@@ -60,6 +62,8 @@ def lambda_creation(config={}):
         return True
     
     except Exception as e:
+        msg = 'lambda_creation failure: {}'.format(e)
+        logging.error(msg)
         return False
 
 
@@ -73,7 +77,8 @@ def parse_config():
         return config
     
     except Exception as e:
-        logging.error('parse_config failure: {}'.format(e))
+        msg = 'parse_config failure: {}'.format(e)
+        logging.error(msg)
         return False
 
 
@@ -85,10 +90,12 @@ def _test_lambda_packer():
     try:
         config = parse_config()
         lambda_packer(config)
-        return True
+        return [True, '']
 
     except Exception as e:
-        return False
+        msg = '_test_lambda_packer failure: {}'.format(e)
+        logging.error(msg)
+        return [False, msg]
 
 
 def _test_lambda_creation():
@@ -97,10 +104,13 @@ def _test_lambda_creation():
     :return:
     """
     try:
-        return True
+        raise Exception('uhoh!')
+        return [True, '']
     
     except Exception as e:
-        return False
+        msg = '_test_lambda_creation failure: {}'.format(e)
+        logging.error(msg)
+        return [False, msg]
 
 
 def _test_parse_config():
@@ -112,11 +122,12 @@ def _test_parse_config():
     try:
         config = parse_config()
         logging.debug('Config parsed, training-job-name set to: {}'.format(config['training-job-name']))
-        return True
+        return [True, '']
     
     except Exception as e:
-        logging.error('_test_parse_config failure: {}'.format(e))
-        return False
+        msg = '_test_parse_config failure: {}'.format(e)
+        logging.error(msg)
+        return [False, msg]
 
 
 if __name__ == '__main__':
@@ -124,11 +135,18 @@ if __name__ == '__main__':
     Parse the config, craft a CloudFormation template, create the Lambda function.
     """
 
-    parse_config_test_result = _test_parse_config()
-    lambda_packer_test_result = _test_lambda_packer()
-    #lambda_creation_test_result = _test_lambda_creation()
+    # TODO Go through this file and change the logger so not everything's a logging.warning() message.
 
-    # If the test have all passed, go ahead with function creation.
+    testresult = []
+    testresult.append(_test_parse_config())
+    testresult.append(_test_lambda_packer())
+    testresult.append(_test_lambda_creation())
+    for testresult, testmsg in testresult:
+        if not testresult:
+            raise Exception('Tests did not pass: {}'.format(testmsg))
+
+
+    # If the tests have all passed, go ahead with function creation.
     
     #lambda_creation(config=parse_config())
 
