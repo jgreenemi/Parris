098f132b83bd05b52def06b8b4a56b123900d019
Joseph Greene <jgreenemi@gmail.com>
2017-12-22 20:50:21 -0800

    Create lambda_packer, test functions, parse_config.

--- setup.py
+++ setup.py
@@ -7,6 +7,48 @@
 import boto3
 import json
 import logging
+import os
+import zipfile
+
+
+def lambda_packer(config={}):
+    """
+    To create the Lambda function we need to pack up a ZIP file to upload. Returns true if the ZIP archive is created,
+    False is something goes wrong.
+    :return:
+    """
+    try:
+        if not config:
+            raise Exception('lambda_packer is missing a config. Needed for setting a Lambda function name.')
+
+        # Create a ZIP archive, with compression. The files in `config/` and `src/` are the only ones we need to include
+        # for the Lambda to function, but in case you have custom needs you can add the necessary directories under this
+        # dict.
+        directories_to_include = ['config', 'src']
+
+        # Create the target directory if it doesn't exist.
+        if not os.path.exists('target'):
+            try:
+                os.makedirs('target')
+            except:
+                raise Exception('Tried to create target/ dir but failed.')
+
+        with zipfile.ZipFile('target/lambdapack-{}.zip'.format(config['training-job-name']), mode='w') as lambdapack:
+            # Crawl directories, and write their files into the ZIP with the structure of foldername/filename.
+            # This should maintain subdirectory heirarchies.
+            for dirname in directories_to_include:
+                for root, dir, filename in os.walk(dirname):
+                    for individual_file in filename:
+                        composite_filename = os.path.join(root, individual_file)
+                        logging.warning('Adding: {}'.format(composite_filename))
+                        lambdapack.write(filename=composite_filename, arcname=composite_filename)
+
+        logging.info('Packed lambdapack to target/lambdapack-{}.zip'.format(config['training-job-name']))
+        return True
+
+    except Exception as e:
+        logging.error('lambda_packer failure: {}'.format(e))
+        return False
 
 
 def lambda_creation(config={}):
@@ -27,9 +69,24 @@ def parse_config():
     :return:
     """
     try:
-        config = {}
+        config = json.load(open('config/config.json'))
         return config
     
+    except Exception as e:
+        logging.error('parse_config failure: {}'.format(e))
+        return False
+
+
+def _test_lambda_packer():
+    """
+    Try to pack up a Lambdapack with the current config.
+    :return:
+    """
+    try:
+        config = parse_config()
+        lambda_packer(config)
+        return True
+
     except Exception as e:
         return False
 
@@ -53,9 +110,12 @@ def _test_parse_config():
     :return:
     """
     try:
+        config = parse_config()
+        logging.debug('Config parsed, training-job-name set to: {}'.format(config['training-job-name']))
         return True
     
     except Exception as e:
+        logging.error('_test_parse_config failure: {}'.format(e))
         return False
 
 
@@ -65,10 +125,11 @@ if __name__ == '__main__':
     """
 
     parse_config_test_result = _test_parse_config()
-    lambda_creation_test_result = _test_lambda_creation()
+    lambda_packer_test_result = _test_lambda_packer()
+    #lambda_creation_test_result = _test_lambda_creation()
 
     # If the test have all passed, go ahead with function creation.
     
-    lambda_creation(config=parse_config())
+    #lambda_creation(config=parse_config())
 
     logging.info('setup.py finished.')
